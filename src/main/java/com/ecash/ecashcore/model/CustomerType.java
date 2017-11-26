ea3f7a965638ac5652a4b5ab7a61484cfca347c5
HungThe <hungntheit@gmail.com>
2017-11-26 23:42:52 +0700

    Add   @JsonBackReference to CustomerType

--- src/main/java/com/ecash/ecashcore/model/CustomerType.java
+++ src/main/java/com/ecash/ecashcore/model/CustomerType.java
@@ -6,10 +6,14 @@ import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 @Table(name = "customer_type")
 public class CustomerType extends Type {
+  
   @OneToMany(mappedBy = "customerType")
+  @JsonBackReference
   private List<Customer> customer;
 
   public List<Customer> getCustomer() {
