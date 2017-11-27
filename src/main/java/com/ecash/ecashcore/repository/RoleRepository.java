e15309e57e4ac486cc9d65cc10aafaf0b85cdb61
Trinh Nguyen <nntrinh@tma.com.vn>
2017-11-28 01:30:17 +0700

    Create user history

--- src/main/java/com/ecash/ecashcore/repository/RoleRepository.java
+++ src/main/java/com/ecash/ecashcore/repository/RoleRepository.java
@@ -1,12 +1,13 @@
 package com.ecash.ecashcore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.ecash.ecashcore.model.Role;
 
 @RestResource(exported = false)
-public interface RoleRepository extends JpaRepository<Role, String> {
+public interface RoleRepository extends JpaRepository<Role, String>, QueryDslPredicateExecutor<Role> {
 
   Role findByName(String name);
 
