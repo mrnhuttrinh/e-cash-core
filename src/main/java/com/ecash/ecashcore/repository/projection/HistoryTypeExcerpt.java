620ae05490c9c8030e48cdc8a6cb2cac3e3f8de8
Trinh Nguyen <nntrinh@tma.com.vn>
2017-12-06 00:37:57 +0700

    add method help to get field on historyType

--- src/main/java/com/ecash/ecashcore/repository/projection/HistoryTypeExcerpt.java
+++ src/main/java/com/ecash/ecashcore/repository/projection/HistoryTypeExcerpt.java
@@ -3,7 +3,17 @@ package com.ecash.ecashcore.repository.projection;
 import org.springframework.data.rest.core.config.Projection;
 
 import com.ecash.ecashcore.model.HistoryType;
+import com.ecash.ecashcore.model.User;
 
 @Projection(name = "custom", types = HistoryType.class)
 public interface HistoryTypeExcerpt extends BaseExcerpt {
+  public String getType();
+  
+  public String getName();
+  
+  public String getDescription();
+  
+  public String getStatus();
+  
+  public String getDisplayName();
 }
