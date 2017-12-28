5b7ea37991cc40e3d30638a964daa31aa17af221
Duy Viec <duyviec@outlook.com>
2017-12-28 10:47:11 +0700

    temp

--- src/main/java/com/ecash/ecashcore/repository/EWalletTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ecash.ecashcore.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ecash.ecashcore.model.wallet.EWalletType;
-
-public interface EWalletTypeRepository extends JpaRepository<EWalletType, String> {
-  EWalletType findByTypeCode(String typeCode);
-}
