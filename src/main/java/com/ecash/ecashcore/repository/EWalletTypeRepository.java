254e0bb054da95a6e9ebcad52e9ce19d924d3597
Duy Viec <duyviec@outlook.com>
2017-12-31 22:32:11 +0700

    rebase

--- src/main/java/com/ecash/ecashcore/repository/EWalletTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ecash.ecashcore.repository;
-
-import com.ecash.ecashcore.model.wallet.EWalletType;
-import com.ecash.ecashcore.model.wallet.QEWalletType;
-
-public interface EWalletTypeRepository extends BaseQuerydslRepository<EWalletType, String, QEWalletType> {
-
-  EWalletType findByTypeCode(String typeCode);
-}
