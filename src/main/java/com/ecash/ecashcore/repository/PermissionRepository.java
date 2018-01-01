02e2941a08b9f31b28d6e25e53b3de3388b73c05
Trinh Nguyen <nntrinh@tma.com.vn>
2018-01-02 00:22:26 +0700

    Update role permission

--- src/main/java/com/ecash/ecashcore/repository/PermissionRepository.java
+++ src/main/java/com/ecash/ecashcore/repository/PermissionRepository.java
@@ -1,8 +1,11 @@
 package com.ecash.ecashcore.repository;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+
 import com.ecash.ecashcore.model.cms.Permission;
 import com.ecash.ecashcore.model.cms.QPermission;
 
+@PreAuthorize(value = "hasRole('ROLE_ADMIN')")
 public interface PermissionRepository extends BaseQuerydslRepository<Permission, String, QPermission> {
 
   Permission findByName(String name);
