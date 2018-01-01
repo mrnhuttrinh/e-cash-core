664ed778cf6bc37e7c0d5edd37c9815decaff2a0
Duy Viec <duyviec@outlook.com>
2018-01-01 11:38:03 +0700

    Remove customize

--- src/main/java/com/ecash/ecashcore/repository/TransactionRepository.java
+++ src/main/java/com/ecash/ecashcore/repository/TransactionRepository.java
@@ -28,42 +28,5 @@ public interface TransactionRepository
   List<Transaction> findByRelatedTransactionId(@Param("transactionId") String transactionId);
 
   public List<Transaction> findByDateBetweenAndCardIsNotNull(Date fromDate, Date toDate);
-
-  @Override
-  default public void customize(QuerydslBindings bindings, QTransaction transaction)
-  {
-    bindings.bind(String.class).first(new SingleValueBinding<StringPath, String>()
-    {
-      @Override
-      public Predicate bind(StringPath path, String value)
-      {
-        return path.containsIgnoreCase(value);
-      }
-    });
-
-    bindings.bind(Date.class)
-        .all((final DateTimePath<Date> path, final Collection<? extends Date> values) -> {
-          final List<? extends Date> dates = new ArrayList<>(values);
-          Collections.sort(dates);
-          if (dates.size() == 2)
-          {
-            return path.between(dates.get(0), dates.get(1));
-          }
-
-          // Expressions "to_char" only work in postgresql
-          return Expressions.stringTemplate("to_char({0}, {1})", path, DateTimeUtils.DEFAULT_FORMAT)
-              .likeIgnoreCase(DateTimeUtils.toDefaultFormatString(dates.get(0)));
-        });
-
-    bindings.bind(Double.class)
-        .all((final NumberPath<Double> path, final Collection<? extends Double> values) -> {
-          final List<? extends Double> doubles = new ArrayList<>(values);
-          Collections.sort(doubles);
-          if (doubles.size() == 2)
-          {
-            return path.between(doubles.get(0), doubles.get(1));
-          }
-          return path.eq(doubles.get(0));
-        });
-  }
+  
 }
