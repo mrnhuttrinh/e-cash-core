a409693bdebb7c4c9ae8473b8de69a7fdc3dd03a
HungThe <hungntheit@gmail.com>
2018-01-23 15:05:30 +0700

    update card permission for card list.

--- src/main/java/com/ecash/ecashcore/repository/CardRepository.java
+++ src/main/java/com/ecash/ecashcore/repository/CardRepository.java
@@ -1,11 +1,20 @@
 package com.ecash.ecashcore.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
+
 import com.ecash.ecashcore.model.cms.Card;
 import com.ecash.ecashcore.model.cms.QCard;
+import com.querydsl.core.types.Predicate;
 
 public interface CardRepository extends BaseQuerydslRepository<Card, String, QCard> {
 
   Card findByCardCode(String cardCode);
 
   Card findByCardNumber(String cardNumber);
+  
+  @Override
+  @PreAuthorize(value = "hasPermission(null, 'CARD_LIST/VIEW')")
+  public Page<Card> findAll(Predicate predicate, Pageable pageable);
 }
