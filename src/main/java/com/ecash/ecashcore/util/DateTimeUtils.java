6bc7f3ddcb65c1cf97f77a26f70c8dc61d459a61
Duy Viec <duyviec@outlook.com>
2018-01-26 16:45:38 +0700

    HF Statement Time.

--- src/main/java/com/ecash/ecashcore/util/DateTimeUtils.java
+++ src/main/java/com/ecash/ecashcore/util/DateTimeUtils.java
@@ -1,7 +1,9 @@
 package com.ecash.ecashcore.util;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
@@ -10,7 +12,8 @@ import org.joda.time.format.DateTimeFormatter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public final class DateTimeUtils {
+public final class DateTimeUtils
+{
   private static final Logger LOG = LoggerFactory.getLogger(DateTimeUtils.class);
   public static final String DEFAULT_FORMAT = "yyyy-MM-dd";
 
@@ -18,60 +21,80 @@ public final class DateTimeUtils {
 
   public static final String DEFAULT_FORMAT_SHORT_DATE = "yyyyMMdd";
 
-  public static DateTime parseDate(String dateString, String datePatternParam, DateTimeZone timeZone) {
+  public static DateTime parseDate(String dateString, String datePatternParam,
+      DateTimeZone timeZone)
+  {
     DateTime dt = null;
     String datePattern = datePatternParam;
-    try {
-      if (StringUtils.isNullOrEmpty(datePattern)) {
+    try
+    {
+      if (StringUtils.isNullOrEmpty(datePattern))
+      {
         datePattern = DEFAULT_FORMAT;
       }
 
-      if (!StringUtils.isNullOrEmpty(dateString)) {
+      if (!StringUtils.isNullOrEmpty(dateString))
+      {
         DateTimeFormatter dtf = DateTimeFormat.forPattern(datePattern);
 
-        if (timeZone != null) {
+        if (timeZone != null)
+        {
           dt = dtf.withZone(timeZone).parseDateTime(dateString);
-        } else {
+        }
+        else
+        {
           dt = dtf.parseDateTime(dateString);
         }
       }
-    } catch (Exception e) {
+    }
+    catch (Exception e)
+    {
       LOG.error(e.getMessage(), e);
     }
 
     return dt;
   }
 
-  public static String toDefaultFormatString(Date date) {
-    if (date == null) {
+  public static String toDefaultFormatString(Date date)
+  {
+    if (date == null)
+    {
       return null;
     }
     SimpleDateFormat formatter = new SimpleDateFormat(DEFAULT_FORMAT);
     return formatter.format(date);
   }
 
-  public static String datetoString(Date date, final String inputFormat) {
+  public static String datetoString(Date date, final String inputFormat)
+  {
     String dateFormat = inputFormat;
-    if (StringUtils.isNullOrEmpty(dateFormat)) {
+    if (StringUtils.isNullOrEmpty(dateFormat))
+    {
       dateFormat = DEFAULT_FORMAT;
     }
     SimpleDateFormat formatter = new SimpleDateFormat(dateFormat);
     return formatter.format(date);
   }
 
-  @SuppressWarnings("deprecation")
-  public static Date convertToStartOfDay(Date date) {
-    date.setHours(0);
-    date.setMinutes(0);
-    date.setSeconds(0);
-    return date;
+  public static Date convertToStartOfDay(Date date)
+  {
+    Calendar cal = Calendar.getInstance();
+    cal.setTime(date);
+    cal.set(Calendar.HOUR_OF_DAY, 0);
+    cal.set(Calendar.MINUTE, 0);
+    cal.set(Calendar.SECOND, 0);
+    cal.set(Calendar.MILLISECOND, 0);
+    return cal.getTime();
   }
 
-  @SuppressWarnings("deprecation")
-  public static Date convertToEndOfDay(Date date) {
-    date.setHours(23);
-    date.setMinutes(59);
-    date.setSeconds(59);
-    return date;
+  public static Date convertToEndOfDay(Date date)
+  {
+    Calendar cal = Calendar.getInstance();
+    cal.setTime(date);
+    cal.set(Calendar.HOUR_OF_DAY, 0);
+    cal.set(Calendar.MINUTE, 0);
+    cal.set(Calendar.SECOND, 0);
+    cal.set(Calendar.MILLISECOND, 0);
+    return new Date(cal.getTime().getTime() + TimeUnit.DAYS.toMillis(1));
   }
 }
